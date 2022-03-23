4c2c56936921670208d1467b9cf174386d2fe60d
Burabari <burabaridev@gmail.com>
2022-03-23 12:34:05 +0100

    initial commit

---
+++ src/main/java/com/burabari/workerbee/WorkerbeeApplication.java
@@ -0,0 +1,13 @@
+package com.burabari.workerbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerbeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerbeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burabari/workerbee/WorkerbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burabari.workerbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerbeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
