970972ee8bd6afeafd382821aa4440e2c56cc676
Burabari <burabaridev@gmail.com>
2022-04-03 02:01:27 +0100

    change the name of Application class to WorkerBee

--- src/main/java/com/burabari/workerbee/WorkerBee.java
+++ src/main/java/com/burabari/workerbee/WorkerBee.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkerbeeApplication {
+public class WorkerBee {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkerbeeApplication.class, args);
+		SpringApplication.run(WorkerBee.class, args);
 	}
 
 }
