15ca6b3e0b401175f25b6b0172ed7a2efc3450e8
Burabari <burabaridev@gmail.com>
2022-04-04 10:16:08 +0100

    add unit test for ClientService.create method

--- src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
+++ src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
@@ -4,11 +4,16 @@
  */
 package com.burabari.workerbee.services;
 
+import com.burabari.workerbee.models.Client;
 import com.burabari.workerbee.repos.ClientsRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
@@ -25,4 +30,18 @@ public class ClientServiceTest {
     @InjectMocks
     private ClientService service;
     
+    @Test
+    void create() {
+        Client client = new Client("abc-34def", "ABC Inc.", new ArrayList<>(), new ArrayList<>());
+        Client createdClient = new Client("abc-34def", "ABC Inc.", new ArrayList<>(), new ArrayList<>());
+        createdClient.setId(1L);
+        
+        when(repo.save(client)).thenReturn(createdClient);
+
+        Client created = service.create(client);
+
+        Assertions.assertThat(created).isNotNull();
+        Assertions.assertThat(created.getId()).isNotNull();
+        Assertions.assertThat(created.getClientId()).isEqualTo(client.getClientId());
+    }
 }
