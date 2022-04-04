ef57a1a9751b150bba547f70078545813dab7a88
Burabari <burabaridev@gmail.com>
2022-04-04 07:21:40 +0100

    add Int. tests for StaffController.delete method

--- src/main/java/com/burabari/workerbee/controllers/StaffController.java
+++ src/main/java/com/burabari/workerbee/controllers/StaffController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,49 +27,62 @@ import org.springframework.web.bind.annotation.RestController;
  * @author codebase
  */
 @RequestMapping(
-        path = {"/api/staff"}, 
+        path = {"/api/staff"},
         consumes = {"application/json"},
         produces = {"application/json"})
 @RestController
 public class StaffController {
-    
+
     private final StaffService service;
-    
+
     @Autowired
-    public StaffController(StaffService service){
+    public StaffController(StaffService service) {
         this.service = service;
     }
-    
+
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<StaffDTO> createStaff(@RequestBody Staff staff){
+    public ResponseEntity<StaffDTO> createStaff(@RequestBody Staff staff) {
         StaffDTO staffDto = service.create(staff);
         return ResponseEntity.created(
-                URI.create("/api/staff/"+staffDto.getId()))
+                URI.create("/api/staff/" + staffDto.getId()))
                 .body(staffDto);
     }
-    
+
     @GetMapping(params = {"id"})
-    public ResponseEntity<StaffDTO> getById(@RequestParam(name = "id") long id){
+    public ResponseEntity<StaffDTO> getById(@RequestParam(name = "id") long id) {
         Optional<StaffDTO> staffDto = service.getById(id);
         return ResponseEntity.of(staffDto);
     }
-    
+
     @GetMapping
-    public ResponseEntity<List<StaffDTO>> get10Staff(){
+    public ResponseEntity<List<StaffDTO>> get10Staff() {
         List<StaffDTO> next10 = service.getNext10(0);
         return ResponseEntity.ok(next10);
     }
-    
+
     @PutMapping
-    public ResponseEntity updateStaff(@RequestBody(required = true) Staff staff){
+    public ResponseEntity updateStaff(@RequestBody(required = true) Staff staff) {
         try {
             boolean updated = service.update(staff);
             if (updated) {
                 return ResponseEntity.noContent().build();
             }
+            return ResponseEntity.notFound().build();
         } catch (Exception ex) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteStaff(@RequestParam(name = "id") Long id) {
+        try {
+            boolean deleted = service.delete(id);
+            if (deleted) {
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
