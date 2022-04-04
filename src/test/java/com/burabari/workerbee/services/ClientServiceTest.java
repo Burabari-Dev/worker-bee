98cd68ea6e6cf01cb27ac0f898dda6ffb8f987d8
Burabari <burabaridev@gmail.com>
2022-04-04 09:59:36 +0100

    setup ClientServiceTest

---
+++ src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.burabari.workerbee.services;
+
+import com.burabari.workerbee.repos.ClientsRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ *
+ * @author codebase
+ */
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceTest {
+    
+    @Mock
+    private ClientsRepo repo;
+    @Mock
+    private ObjectMapper mapper;
+    @InjectMocks
+    private ClientService service;
+    
+}
