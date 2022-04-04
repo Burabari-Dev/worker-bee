e037e7c6e5a6320b6d1f7c8e7630cb630260709b
Burabari <burabaridev@gmail.com>
2022-04-04 14:46:43 +0100

    add unit test method ClientServiceTest.getPage

--- src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
+++ src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
@@ -5,9 +5,12 @@
 package com.burabari.workerbee.services;
 
 import com.burabari.workerbee.models.Client;
+import com.burabari.workerbee.models.Staff;
 import com.burabari.workerbee.repos.ClientsRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 /**
  *
@@ -43,5 +50,23 @@ public class ClientServiceTest {
         Assertions.assertThat(created).isNotNull();
         Assertions.assertThat(created.getId()).isNotNull();
         Assertions.assertThat(created.getClientId()).isEqualTo(client.getClientId());
+    }
+    
+    @Test
+    void getPage(){
+        int pageNo = 3;
+        Client[] clientArr = new Client[10];
+        for(int i = 0; i < clientArr.length; i++){
+            clientArr[i] = new Client("abc-"+i+"4def", "", 
+                    new ArrayList<>(), new ArrayList<>());
+        }
+        List<Client> clientList = Arrays.asList(clientArr);
+        
+        Page<Client> page = new PageImpl<>(clientList, Pageable.ofSize(10), 50);
+        when(repo.findAll(PageRequest.of(pageNo, 10))).thenReturn(page);
+        
+        List<Client> clients = service.getPage(pageNo, 10);
+        
+        Assertions.assertThat(clients.size()).isBetween(0, 10);
     }
 }
