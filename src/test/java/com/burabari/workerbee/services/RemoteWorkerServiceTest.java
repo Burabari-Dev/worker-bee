fa9f448655f8211af9891a4e717957cc87dbcc89
Burabari <burabaridev@gmail.com>
2022-04-05 13:12:53 +0100

    add unit tests for RemoteWorkerService.create method

--- src/test/java/com/burabari/workerbee/services/RemoteWorkerServiceTest.java
+++ src/test/java/com/burabari/workerbee/services/RemoteWorkerServiceTest.java
@@ -4,11 +4,21 @@
  */
 package com.burabari.workerbee.services;
 
+import com.burabari.workerbee.models.RemoteWorker;
+import com.burabari.workerbee.models.dtos.RemoteWorkerDTO;
+import com.burabari.workerbee.models.enums.UserType;
 import com.burabari.workerbee.repos.RemoteWorkersRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.isA;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
@@ -23,4 +33,29 @@ public class RemoteWorkerServiceTest {
     private ObjectMapper mapper;
     @InjectMocks
     RemoteWorkerService service;
+    
+    @Test
+    void create(){
+        RemoteWorker worker = new RemoteWorker();
+        RemoteWorkerDTO dto = new RemoteWorkerDTO();
+        worker.setEmail("worker@email.com");
+        dto.setEmail("worker@email.com");
+        
+        when(repo.save(worker)).thenReturn(worker);
+        when(mapper.convertValue(worker, RemoteWorkerDTO.class)).thenReturn(dto);
+        
+        Optional<RemoteWorkerDTO> opt = service.create(worker);
+        
+        Assertions.assertThat(opt.isPresent()).isTrue();
+        Assertions.assertThat(opt.get().getEmail()).isEqualTo(dto.getEmail());
+    }
+    
+    @Test
+    void create_With_Null_Worker(){
+        RemoteWorker worker = null;
+        
+        Optional<RemoteWorkerDTO> opt = service.create(worker);
+        
+        Assertions.assertThat(opt.isPresent()).isFalse();
+    }
 }
