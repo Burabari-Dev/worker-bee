0c2e7780e49ec9ed9445b42fca2058c2220de4e0
Burabari <burabaridev@gmail.com>
2022-04-05 06:19:34 +0100

    add unit test for ClientService.update method

--- src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
+++ src/test/java/com/burabari/workerbee/services/ClientServiceTest.java
@@ -5,7 +5,6 @@
 package com.burabari.workerbee.services;
 
 import com.burabari.workerbee.models.Client;
-import com.burabari.workerbee.models.Staff;
 import com.burabari.workerbee.repos.ClientsRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
@@ -30,20 +29,20 @@ import org.springframework.data.domain.Pageable;
  */
 @ExtendWith(MockitoExtension.class)
 public class ClientServiceTest {
-    
+
     @Mock
     private ClientsRepo repo;
     @Mock
     private ObjectMapper mapper;
     @InjectMocks
     private ClientService service;
-    
+
     @Test
     void create() {
         Client client = new Client("abc-34def", "ABC Inc.", new ArrayList<>(), new ArrayList<>());
         Client createdClient = new Client("abc-34def", "ABC Inc.", new ArrayList<>(), new ArrayList<>());
         createdClient.setId(1L);
-        
+
         when(repo.save(client)).thenReturn(createdClient);
 
         Client created = service.create(client);
@@ -52,36 +51,80 @@ public class ClientServiceTest {
         Assertions.assertThat(created.getId()).isNotNull();
         Assertions.assertThat(created.getClientId()).isEqualTo(client.getClientId());
     }
-    
+
     @Test
-    void getPage(){
+    void getPage() {
         int pageNo = 3;
         Client[] clientArr = new Client[10];
-        for(int i = 0; i < clientArr.length; i++){
-            clientArr[i] = new Client("abc-"+i+"4def", "", 
+        for (int i = 0; i < clientArr.length; i++) {
+            clientArr[i] = new Client("abc-" + i + "4def", "",
                     new ArrayList<>(), new ArrayList<>());
         }
         List<Client> clientList = Arrays.asList(clientArr);
-        
+
         Page<Client> page = new PageImpl<>(clientList, Pageable.ofSize(10), 50);
         when(repo.findAll(PageRequest.of(pageNo, 10))).thenReturn(page);
-        
+
         List<Client> clients = service.getPage(pageNo, 10);
-        
+
         Assertions.assertThat(clients.size()).isBetween(0, 10);
     }
-    
+
     @Test
-    void findById(){
+    void findById() {
         long id = 1L;
         Client dbClient = new Client("abc-34def", "ABC Inc.", new ArrayList<>(), new ArrayList<>());
         dbClient.setId(id);
         Optional<Client> opt = Optional.of(dbClient);
-        
+
         when(repo.findById(id)).thenReturn(opt);
-        
+
         Optional<Client> byId = service.findById(id);
         Assertions.assertThat(byId).isNotNull();
         Assertions.assertThat(byId).isInstanceOf(Optional.class);
+    }
+
+    @Test
+    void update() {
+        Client newClient = new Client("def-44abc", "DEF Inc.", new ArrayList<>(), new ArrayList<>());
+        newClient.setId(1L);
+
+        when(repo.existsById(newClient.getId())).thenReturn(true);
+        when(repo.save(newClient)).thenReturn(newClient);
+
+        boolean updated = false;
+        try {
+            updated = service.update(newClient);
+        } catch (IllegalArgumentException e) {}
+
+        Assertions.assertThat(updated).isTrue();
+    }
+
+    @Test
+    void update_With_Error() {
+        Client newClient = new Client("def-44abc", "DEF Inc.", new ArrayList<>(), new ArrayList<>());
+        newClient.setId(1L);
+
+        when(repo.existsById(newClient.getId())).thenReturn(false);
+//        when(repo.save(newClient)).thenReturn(newClient);
+
+        boolean updated = false;
+        try {
+            updated = service.update(newClient);
+        } catch (IllegalArgumentException e) {}
+
+        Assertions.assertThat(updated).isFalse();
+    }
+
+    @Test
+    void update_With_No_Data() {
+        Client newClient = null;
+
+        boolean updated = false;
+        try {
+            updated = service.update(newClient);
+        } catch (IllegalArgumentException e) { }
+
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class);
     }
 }
