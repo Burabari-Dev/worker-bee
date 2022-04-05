9b1a16d4560d488ce33ca026711753f8bd59aef7
Burabari <burabaridev@gmail.com>
2022-04-05 05:51:38 +0100

    add getClientById method to ClientController

--- src/main/java/com/burabari/workerbee/controllers/ClientController.java
+++ src/main/java/com/burabari/workerbee/controllers/ClientController.java
@@ -8,14 +8,17 @@ import com.burabari.workerbee.models.Client;
 import com.burabari.workerbee.services.ClientService;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -49,5 +52,12 @@ public class ClientController {
             @RequestParam(name = "size", required = false, defaultValue = "10")int size){
         List<Client> clients = service.getPage(page, size);
         return ResponseEntity.ok(clients);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(
+            @PathVariable(required = true) long id){
+        Optional<Client> clientOpt = service.findById(id);
+        return ResponseEntity.of(clientOpt);
     }
 }
