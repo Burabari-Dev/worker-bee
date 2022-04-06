e2f432c6c06efb1e3245419b4ca55a760ca36acc
Burabari <burabaridev@gmail.com>
2022-04-06 23:42:55 +0100

    add Int. tests for ProjectController.createProject method

--- src/test/java/com/burabari/workerbee/controllers/ProjectControllerIT.java
+++ src/test/java/com/burabari/workerbee/controllers/ProjectControllerIT.java
@@ -4,11 +4,19 @@
  */
 package com.burabari.workerbee.controllers;
 
+import com.burabari.workerbee.models.Project;
 import com.burabari.workerbee.services.ProjectService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.when;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  *
@@ -21,5 +29,52 @@ public class ProjectControllerIT {
     private ProjectService service;
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
     
+    @Test
+    void createProject_Should_Return_Status_201() throws Exception{
+        long clientId = 1L;
+        Project project = new Project();
+        project.setProjectId("abc-123");
+        project.setTitle("Project 1");
+        String json = mapper.writeValueAsString(project);
+        Optional opt = Optional.of(project);
+        
+        when(service.create(clientId, project)).thenReturn(opt);
+        
+        mockMvc.perform(post("/api/projects")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("clientId", ""+clientId)
+                .content(json))
+                .andExpect(status().isCreated());
+    }
+    
+    @Test
+    void createProject_With_Invalid_ClientID_Should_Return_Status_400() throws Exception{
+        long clientId = 0L;
+        Project project = new Project();
+        project.setProjectId("abc-123");
+        project.setTitle("Project 1");
+        String json = mapper.writeValueAsString(project);
+        
+        mockMvc.perform(post("/api/projects")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("clientId", ""+clientId)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    void createProject_Without_Data_Should_Return_Status_400() throws Exception{
+        long clientId = 1L;
+        Project project = null;
+        String json = "";
+        
+        mockMvc.perform(post("/api/projects")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("clientId", ""+clientId)
+                .content(json))
+                .andExpect(status().isBadRequest());
+    }
 }
