4448450c72467f44b52b168bd2ae1071962c9256
Burabari <burabaridev@gmail.com>
2022-04-06 21:47:49 +0100

    add unit tests for ProjectService.create method

--- src/test/java/com/burabari/workerbee/services/ProjectServiceTest.java
+++ src/test/java/com/burabari/workerbee/services/ProjectServiceTest.java
@@ -4,12 +4,21 @@
  */
 package com.burabari.workerbee.services;
 
+import com.burabari.workerbee.models.Client;
+import com.burabari.workerbee.models.Project;
 import com.burabari.workerbee.repos.ClientsRepo;
 import com.burabari.workerbee.repos.ProjectsRepo;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  *
@@ -24,4 +33,33 @@ public class ProjectServiceTest {
     @InjectMocks
     private ProjectService service;
     
+    @Test
+    void create(){
+        long id = 1L;
+        Project project = new Project();
+        Client dbClient = new Client();
+        dbClient.setId(id);
+        dbClient.setProjects(new HashSet<>(new ArrayList<Project>()));
+        Optional<Client> opt = Optional.of(dbClient);
+        
+        when(clientRepo.findById(id)).thenReturn(opt);
+        when(clientRepo.save(dbClient)).thenReturn(any());
+        
+        boolean created = service.create(id, project);
+        
+        Assertions.assertThat(created).isTrue();
+    }
+    
+    @Test
+    void create_With_Bad_Clinet_Id(){
+        long id = 5L;
+        Project project = new Project();
+        Optional<Client> opt = Optional.empty();
+        
+        when(clientRepo.findById(id)).thenReturn(opt);
+        
+        boolean created = service.create(id, project);
+        
+        Assertions.assertThat(created).isFalse();
+    }
 }
