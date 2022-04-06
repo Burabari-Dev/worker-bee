a8c65b091c8d9050a3ff9bffc4056b66c1b7cea9
Burabari <burabaridev@gmail.com>
2022-04-06 23:43:37 +0100

    add createProject method to ProjectController

--- src/main/java/com/burabari/workerbee/controllers/ProjectController.java
+++ src/main/java/com/burabari/workerbee/controllers/ProjectController.java
@@ -4,10 +4,17 @@
  */
 package com.burabari.workerbee.controllers;
 
+import com.burabari.workerbee.models.Project;
 import com.burabari.workerbee.services.ProjectService;
+import java.net.URI;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,5 +30,23 @@ public class ProjectController {
     
     @Autowired
     private ProjectService service;
+    
+    @PostMapping
+    public ResponseEntity<Project> createProject(
+            @RequestParam(name = "clientId", required = true) long clientId, 
+            @RequestBody(required = true) Project project) {
+        
+        if(clientId == 0 || project == null)
+            return ResponseEntity.badRequest().build();
+        
+        Optional<Project> created = service.create(clientId, project);
+        
+        if(created.isEmpty())
+            return ResponseEntity.badRequest().build();
+        else
+            return ResponseEntity.created(
+                    URI.create("/api/projects/"+created.get().getProjectId()))
+                    .build();
+    }
     
 }
