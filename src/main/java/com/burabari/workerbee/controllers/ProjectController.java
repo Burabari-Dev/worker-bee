1c729cf3d1becb4e8cbbece5e2e2f6bcddc25047
Burabari <burabaridev@gmail.com>
2022-04-07 05:09:28 +0100

    add findByProjectId method to ProjectController

--- src/main/java/com/burabari/workerbee/controllers/ProjectController.java
+++ src/main/java/com/burabari/workerbee/controllers/ProjectController.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,26 +29,38 @@ import org.springframework.web.bind.annotation.RestController;
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProjectController {
-    
+
     @Autowired
     private ProjectService service;
-    
+
     @PostMapping
     public ResponseEntity<Project> createProject(
-            @RequestParam(name = "clientId", required = true) long clientId, 
+            @RequestParam(name = "clientId", required = true) long clientId,
             @RequestBody(required = true) Project project) {
-        
-        if(clientId == 0 || project == null)
+
+        if (clientId == 0 || project == null) 
             return ResponseEntity.badRequest().build();
-        
+
         Optional<Project> created = service.create(clientId, project);
-        
-        if(created.isEmpty())
+
+        if (created.isEmpty()) {
             return ResponseEntity.badRequest().build();
-        else
+        } else {
             return ResponseEntity.created(
-                    URI.create("/api/projects/"+created.get().getProjectId()))
+                    URI.create("/api/projects/" + created.get().getProjectId()))
                     .build();
+        }
     }
-    
+
+    @GetMapping("/{projId}")
+    public ResponseEntity<Project> findByProjectId(
+            @PathVariable(required = true) String projId) {
+        
+        if (projId == null || projId.isBlank())
+            return ResponseEntity.badRequest().build();
+        
+        Optional<Project> opt = service.findByProjectId(projId);
+        return ResponseEntity.of(opt);
+    }
+
 }
