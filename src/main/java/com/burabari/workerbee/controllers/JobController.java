51009bdf87ed072e15454ffe6dc163b6b630b11e
Burabari <burabaridev@gmail.com>
2022-04-08 11:20:58 +0100

    create JobController

---
+++ src/main/java/com/burabari/workerbee/controllers/JobController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.burabari.workerbee.controllers;
+
+import com.burabari.workerbee.services.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author codebase
+ */
+@RestController
+@RequestMapping(
+        path = "/api/jobs",
+        consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE}
+)
+public class JobController {
+    
+    @Autowired
+    private JobService service;
+    
+}
